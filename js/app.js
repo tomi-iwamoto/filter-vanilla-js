20ec4e2b69ee5e9d1cd6d459b2075656e0768c09
Tomomi <58757939+tomi-design@users.noreply.github.com>
2021-01-31 23:57:58 -0500

    Getting images using queryselectors

---
+++ js/app.js
@@ -0,0 +1,20 @@
+let allButton = document.getElementById('all');
+let cakesButton = document.getElementById('cakes');
+let cupcakesButton = document.getElementById('cupcakes');
+let sweetsButton = document.getElementById('sweets');
+
+/*---images---*/
+let cakesImg = document.querySelector('.cakes');
+let cupcakesImg = document.querySelector('.cupcakes');
+let sweetsImg = document.querySelector('.sweets');
+let allImg = document.querySelector('.all');
+
+
+//when I click the cakes button the following function will be executed. 
+cakesButton.addEventListener('click', function);
+
+
+//when i click on cakes button, i want only the cake images to appear, the other ones to disappear
+function showCakes(){
+
+}
