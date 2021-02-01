b57be15f959eb657d59a9e67d149dd1a66260411
Tomomi <58757939+tomi-design@users.noreply.github.com>
2021-02-01 00:48:39 -0500

    Figured out how to use querySelectorAll and added loops to each function for the event listener

--- js/app.js
+++ js/app.js
@@ -4,17 +4,72 @@ let cupcakesButton = document.getElementById('cupcakes');
 let sweetsButton = document.getElementById('sweets');
 
 /*---images---*/
-let cakesImg = document.querySelector('.cakes');
-let cupcakesImg = document.querySelector('.cupcakes');
-let sweetsImg = document.querySelector('.sweets');
-let allImg = document.querySelector('.all');
-
-
-//when I click the cakes button the following function will be executed. 
-cakesButton.addEventListener('click', function);
+let cakesImg = document.querySelectorAll('.cakes');
+let cupcakesImg = document.querySelectorAll('.cupcakes');
+let sweetsImg = document.querySelectorAll('.sweets');
+let allImg = document.querySelectorAll('.all');
 
 
 //when i click on cakes button, i want only the cake images to appear, the other ones to disappear
-function showCakes(){
-
+function showCakes() {
+  let i;
+  for (i = 0; i < cakesImg.length; i++) { //this loop lets the computer know that it's not just the first element with the class of 'cake' that  I want. I want the whole array of it. 
+    cakesImg[i].style.display = "block";
+  }
+  
+   for (i = 0; i < cupcakesImg.length; i++) {
+    cupcakesImg[i].style.display = "none";
+  }
+  
+    for (i = 0; i < sweetsImg.length; i++) {
+    sweetsImg[i].style.display = "none";
+  }
+  
 }
+
+//when I click the cakes button the following function will be executed.
+cakesButton.addEventListener("click", showCakes);
+
+function showCupcakes() {
+   let i;
+  for (i = 0; i < cupcakesImg.length; i++) {
+    cupcakesImg[i].style.display = "block";
+  }
+  
+   for (i = 0; i < cakesImg.length; i++) {
+    cakesImg[i].style.display = "none";
+  }
+  
+    for (i = 0; i < sweetsImg.length; i++) {
+    sweetsImg[i].style.display = "none";
+  }
+}
+
+cupcakesButton.addEventListener("click", showCupcakes);
+
+function showSweets() {
+    let i;
+  for (i = 0; i < sweetsImg.length; i++) {
+    sweetsImg[i].style.display = "block";
+  }
+  
+   for (i = 0; i < cakesImg.length; i++) {
+    cakesImg[i].style.display = "none";
+  }
+  
+    for (i = 0; i < cupcakesImg.length; i++) {
+    cupcakesImg[i].style.display = "none";
+  }
+}
+
+sweetsButton.addEventListener("click", showSweets);
+
+
+function showAll(){
+  let i;
+  for(i=0; i < allImg.length; i++){
+    allImg[i].style.display = "block";
+  }
+}
+
+allButton.addEventListener('click', showAll);
